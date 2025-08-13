ba6dfda184aec0cfd4d81cfdb0a1a23fefd492d7
nandyalasandeepkumar-boop <nandyalasandeepkumar3786@gmail.com>
2025-08-12 19:54:21 -0500

    Create variables.tf

---
+++ infra/aws/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "project" {
+  type    = string
+  default = "iot-sensor-datalake"
+}
